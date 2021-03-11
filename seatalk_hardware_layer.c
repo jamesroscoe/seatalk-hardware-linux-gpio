b8e79310b25ff391f1b798934bc722874d5832a0
James Roscoe <jamesaroscoe@gmail.com>
2021-03-11 07:20:10 -0500

    fix compile errors

--- seatalk_hardware_layer.c
+++ seatalk_hardware_layer.c
@@ -48,7 +48,7 @@ static irqreturn_t rxd_irq_handler(int irq, void *dev_id, struct pt_regs *regs) 
     pr_info("debouncing\n");
   } else {
     // make sure the receiver is not already active
-    if (initiate_seatalk_receive_character(SEATALK_PORT)) {
+    if (seatalk_initiate_receive_character(SEATALK_PORT)) {
       // reset debounce timer
       // schedule receive event for first bit
       hrtimer_start(&hrtimer_rxd, ktime_set(0, BIT_INTERVAL + START_BIT_DELAY), HRTIMER_MODE_REL);
@@ -60,11 +60,11 @@ static irqreturn_t rxd_irq_handler(int irq, void *dev_id, struct pt_regs *regs) 
   return IRQ_HANDLED;
 }
 
-int get_seatalk_hardware_bit_value(int seatalk_port) {
+int seatalk_get_hardware_bit_value(int seatalk_port) {
   return gpio_get_value(GPIO_RXD_PIN) ? GPIO_RX_HIGH_VALUE : GPIO_RX_LOW_VALUE; // normal sense
 }
 
-void set_seatalk_hardware_bit_value(int seatalk_port, int bit_value) {
+void seatalk_set_hardware_bit_value(int seatalk_port, int bit_value) {
   gpio_set_value(GPIO_TXD_PIN, (bit_value == GPIO_TX_HIGH_VALUE) ? 1 : 0); // normal sense
 //#ifdef DEBUG
 //  pr_info("set GPIO_TXD_PIN to %d\n", bit_value);
@@ -82,7 +82,7 @@ static enum hrtimer_restart receive_bit(struct hrtimer *timer) {
     // calculate the wake-up time for the next bit (if any)
     // (done now to limit time lag on very slow machines)
     // reeive a single bit. Return value indicates whether more bits are expected
-    if (seatalk_receive_bit()) {
+    if (seatalk_receive_bit(SEATALK_PORT)) {
       // more bits are expected. Restart the timer
       hrtimer_forward_now(&hrtimer_rxd, ktime_set(0, BIT_INTERVAL));
       return HRTIMER_RESTART;
@@ -100,7 +100,7 @@ static enum hrtimer_restart transmit_bit(struct hrtimer *timer) {
   // (done now to limit time lag on very slow machines)
   hrtimer_forward_now(&hrtimer_txd, ktime_set(0, BIT_INTERVAL));
   // send single bit. Return value indicates whether there are more bits to send
-  if (seatalk_transmit_bit() == 0) {
+  if (seatalk_transmit_bit(SEATALK_PORT) == 0) {
     // no more bits to send. Don't restart. Transmission will wake when more data enqueued
     return HRTIMER_NORESTART;
   } else {
@@ -109,7 +109,7 @@ static enum hrtimer_restart transmit_bit(struct hrtimer *timer) {
   }
 }
 
-void initiate_seatalk_hardware_transmitter(int seatalk_port, int bit_delay) {
+void seatalk_initiate_hardware_transmitter(int seatalk_port, int bit_delay) {
   // reawaken the transmittter. Wait a 10-bit duration as guard time after last char
   // that has been on the bus
   // first stop pending timer (if any)
@@ -118,7 +118,7 @@ void initiate_seatalk_hardware_transmitter(int seatalk_port, int bit_delay) {
   hrtimer_start(&hrtimer_txd, ktime_set(0, BIT_INTERVAL * bit_delay), HRTIMER_MODE_REL);
 }
 
-int init_seatalk_hardware_signal(void) {
+int seatalk_init_hardware_signal(void) {
   // initialize rx
   // reserve GPIO_RXD_PIN (default GPIO 23)
   if (gpio_request(GPIO_RXD_PIN, GPIO_RXD_DESC)) {
@@ -140,7 +140,7 @@ int init_seatalk_hardware_signal(void) {
   // set pin direction to output
   gpio_direction_output(GPIO_TXD_PIN, 1);
   // set at-rest pin value to high
-  set_seatalk_hardware_bit_value(1);
+  seatalk_set_hardware_bit_value(SEATALK_PORT, 1);
   // initialize the transmit timer bit don't start it
   hrtimer_init(&hrtimer_txd, CLOCK_REALTIME, HRTIMER_MODE_REL);
   hrtimer_txd.function = transmit_bit;
@@ -153,7 +153,7 @@ cleanup:
   return -1;
 }
 
-int init_seatalk_hardware_irq(void) {
+int seatalk_init_hardware_irq(void) {
   // hook irq for RxD GPIO pin
   // get IRQ number for input pin
   if ((gpio_rxd_irq = gpio_to_irq(GPIO_RXD_PIN)) < 0) {
@@ -175,7 +175,7 @@ cleanup:
   return -1;
 }
 
-void exit_seatalk_hardware_signal(void) {
+void seatalk_exit_hardware_signal(void) {
   // release RxD and TxD pins
   gpio_free(GPIO_TXD_PIN);
   gpio_free(GPIO_RXD_PIN);
@@ -185,7 +185,7 @@ void exit_seatalk_hardware_signal(void) {
   return;
 }
 
-void exit_seatalk_hardware_irq(void) {
+void seatalk_exit_hardware_irq(void) {
   // release IRQ
   free_irq(gpio_rxd_irq, GPIO_DEVICE_DESC);
 }
